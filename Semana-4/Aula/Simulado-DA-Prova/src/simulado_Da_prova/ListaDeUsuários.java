a77dc4821046047a83e9936eada60627e5280416
LobinhoTroll <gabrielteles931@gmail.com>
2023-11-28 17:49:06 -0300

    continuando a atividade

--- Semana-4/Aula/Simulado-DA-Prova/src/simulado_Da_prova/ListaDeUsuários.java
+++ Semana-4/Aula/Simulado-DA-Prova/src/simulado_Da_prova/ListaDeUsuários.java
@@ -1,5 +1,26 @@
 package simulado_Da_prova;
 
-public class ListaDeUsuários {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class ListaDeUsuários {
+//Attributes
+	private ArrayList<Usuário> ListaDeUsuários;
+//Methods
+	public void criarUsuario(Usuário novoUsuario){
+		
+		this.ListaDeUsuários.add(novoUsuario);
+	}
+	public void removerUsuario() {
+		String usuario;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Informe o email ou nome do usuário a ser excluido: ");
+		usuario = sc.nextLine();		
+		
+		for(int i = 0; i < ListaDeUsuários.size(); i++) {
+			if(ListaDeUsuários.get(i).getNome().equals(usuario)){
+				ListaDeUsuários.remove(i);
+			}
+		}
+	}
 }
